bccbab91169a48fc5b65c07bf1ac7b02bfb5b3df
Michael Jones <michael.jones90@me.com>
2026-07-01 09:45:30 +0100

    extra comments to help explain what DataContainer does

--- HackingWithSwiftUI-FriendFace/Logic/DataContainer.swift
+++ HackingWithSwiftUI-FriendFace/Logic/DataContainer.swift
@@ -8,15 +8,18 @@
 import SwiftData
 import SwiftUI
 
+/// This class is designed to set up a model container and provide an easy way to access the main context, and optionally load sample data.
 @Observable
 @MainActor
 class DataContainer {
+    /// Holds the main ModelContainer instance and is responsible for managing the database.
     let modelContainer: ModelContainer
     
+    /// Computed property for accessing the main database context model from the container. This is used for creating, fetching and saving model objects.
     var context: ModelContext {
         modelContainer.mainContext
     }
-    
+     
     init(includeSampleUsers: Bool = false) {
         let schema = Schema([
             User.self
